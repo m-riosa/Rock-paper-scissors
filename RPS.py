e638a13ac92e1294e0c1cf11099701a0d567e2cc
m-riosa <96079530+m-riosa@users.noreply.github.com>
2022-01-17 20:49:06 -0500

    Create RPS.py

---
+++ RPS.py
@@ -0,0 +1,40 @@
+steps = {}
+
+def player(prev_play, opponent_history=[]):
+    global steps
+
+    n = 3
+
+    if prev_play in ["R", "P", "S"]:
+     opponent_history.append(prev_play)
+
+    hist = opponent_history
+
+    guess = "R"
+    
+    if len(hist) > n:
+     pattern = "".join(hist[-n:])
+
+     if "".join(hist[-(n + 1):]) in steps.keys():
+       steps["".join(hist[-(n + 1):])] += 1
+     else:
+       steps["".join(hist[-(n +1):])] = 1
+
+     possible = [pattern + "R", pattern + "P", pattern + "S"]
+
+     for i in possible:
+       if not i in steps.keys():
+         steps[i] = 0
+
+     predict = max(possible, key = lambda key: steps[key])
+
+     if predict[-1] == "P":
+       guess = "S"
+     if predict[-1] == "R":
+       guess = "P"
+     if predict[-1] == "S":
+       guess = "R"
+
+
+    return guess
+
